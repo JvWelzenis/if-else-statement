8dcb2e3af6a5b310d8f7b8aa2a39c11e870a92cf
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-09 20:22:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 18;
+if (age > 18) {
+    console.log ("je mag naar binnen");
+} else if (age < 18) {
+    console.log ("je mag niet naar binnen");
+} else (age == 18) 
+    console.log ("je mag naar binnen");
+
+const isFemale = true;
+if (isFemale == isFemale) {
+    console.log ('je mag naar binnen');
+} else (isFemale = any) 
+    console.log ('je mag niet naar binnen');
+
+const driverStatus = bob;
+if (driverStatus == bob) {
+    console.log ('je mag rijden');
+} else (driverstatus = any) 
+    console.log ('je mag niet rijden');
+
+// het kwartje is nog niet gevallen bij het leren van Javascript. Ik weet zeker dat dit nog wel komt...
